d5eb6c3cece6a1c13c2c7f80a920359c0526ec4b
ryosms <ryo.sms@gmail.com>
2020-05-27 22:41:53 +0900

    Add ShellScript option

--- aws/validate-template.sh
+++ aws/validate-template.sh
@@ -2,6 +2,7 @@
 
 cd "$(dirname "${0}")" || exit 1
 EXIT_CODE=0
+set -x
 aws cloudformation validate-template --template-body file://vpc.yaml > /dev/null || EXIT_CODE=$?
 aws cloudformation validate-template --template-body file://ecs.yaml > /dev/null || EXIT_CODE=$?
 exit ${EXIT_CODE}
