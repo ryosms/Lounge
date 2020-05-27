c708e839ea20b27756917ce717e050f9be50d7dc
ryosms <ryo.sms@gmail.com>
2020-05-27 22:33:52 +0900

    Mod validate CFn template action

---
+++ aws/validate-template.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${0}")" || exit 1
+EXIT_CODE=0
+aws cloudformation validate-template --template-body file://vpc.yaml > /dev/null || EXIT_CODE=$?
+aws cloudformation validate-template --template-body file://ecs.yaml > /dev/null || EXIT_CODE=$?
+exit ${EXIT_CODE}
